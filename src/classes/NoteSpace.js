315c04cd71ed72179ad2df28212485004652f771
neotje <neotje111@gmail.com>
2022-04-08 22:39:03 +0200

    using uuid

--- src/classes/NoteSpace.js
+++ src/classes/NoteSpace.js
@@ -2,19 +2,27 @@ const { Connection } = require("./Connection")
 const { Note } = require("./Note")
 
 class NoteSpace {
+   id
    name
    notes
    connections
 
    /**
+    * @param {string} id Note space id
     * @param {string} name space name
     * @param {Note[]} notes notes in this space
     * @param {Connection[]} connections connection in this space
     */
-   constructor(name, notes, connections) {
+   constructor(id, name, notes, connections) {
+      this.id = id
       this.name = name
       this.notes = notes
       this.connections = connections
+   }
+
+   createNote(name) {
+      var newNote = new Note(/*name*/)
+      this.notes.push(newNote)
    }
 }
 
--- src/classes/NoteSpaceApp.js
+++ src/classes/NoteSpaceApp.js
@@ -1,5 +1,12 @@
 const { NoteSpace } = require("./NoteSpace")
 const EventEmitter = require("events")
+const { v4: uuidv4 } = require("uuid")
+
+class NoteSpaceDoesNotExist extends Error {
+   constructor() {
+      super("Notespace does not exist.")
+   }
+}
 
 class NoteSpaceApp extends EventEmitter {
    spaces
@@ -7,13 +14,36 @@ class NoteSpaceApp extends EventEmitter {
 
    /**
     * @param {NoteSpace[]} spaces
-    * @param {number} activeSpace active space by index. -1 is none
+    * @param {?string} activeSpace active space by index
     */
    constructor(spaces, activeSpace) {
       super()
 
       this.spaces = spaces
       this.activeSpace = activeSpace
+   }
+
+   /**
+    * @param {string} id Space id to look for.
+    * @throws {NoteSpaceDoesNotExist}
+    * @returns {?NoteSpace}
+    */
+   getSpaceById(id) {
+      return this.spaces[this.getSpaceIndexById(id)]
+   }
+
+   /**
+    * @param {string} id Space id to look for.
+    * @throws {NoteSpaceDoesNotExist}
+    * @returns {number} index of space.
+    */
+   getSpaceIndexById(id) {
+      for (let index = 0; index < this.spaces.length; index++) {
+         const element = this.spaces[index];
+
+         if(space.id == id) return index
+      }
+      throw new NoteSpaceDoesNotExist()
    }
 
    /**
@@ -24,7 +54,7 @@ class NoteSpaceApp extends EventEmitter {
     * @returns {NoteSpace} newly created NoteSpace
     */
    createNoteSpace(name) {
-      var newSpace = new NoteSpace(name, [], [])
+      var newSpace = new NoteSpace(uuidv4(), name, [], [])
       this.spaces.push(newSpace)
 
       /**
@@ -42,55 +72,39 @@ class NoteSpaceApp extends EventEmitter {
     * open space by index
     * 
     * @fires NoteSpaceApp#spaceOpened
-    * @param {number} spaceNum 
+    * @param {string} id 
     * @returns {?NoteSpace} returns activated space.
     */
-   openSpace(spaceNum) {
-      var activatedSpace
-
-      if (spaceNum > -1 && spaceNum < this.spaces.length) {
-         this.activeSpace = spaceNum
-         activatedSpace = this.spaces[spaceNum]
-      } else {
-         this.activeSpace = -1
-      }
+   openSpace(id) {
+      var spaceToActivate = this.getSpaceById(id)
+      this.activeSpace = spaceToActivate.id
 
       /**
        * space opened event.
        * @event NoteSpaceApp#spaceOpened
-       * @type {object}
-       * @property {number} activeSpace space index
-       * @property {?NoteSpace} activatedSpace space object
+       * @type {NoteSpace}
        */
-      this.emit("spaceOpened", {
-         activeSpace: this.activeSpace, 
-         activatedSpace: activatedSpace
-      })
+      this.emit("spaceOpened", spaceToActivate)
    }
 
    /**
     * remove space by index
     * 
     * @fires NoteSpaceApp#spaceRemoved
-    * @param {number} spaceNum 
+    * @param {string} id 
     * @returns {?NoteSpace} removed space.
     */
-   removeSpace(spaceNum) {
-      var removedSpace
-
-      if (spaceNum > -1 && spaceNum < this.spaces.length) {
-         removedSpace = this.spaces.splice(spaceNum, 1)[0]
-      }
+   removeSpace(id) {
+      var spaceIndex = this.getSpaceIndexById(id)
+      removedSpace = this.spaces.splice(spaceIndex, 1)[0]
 
       /**
        * space removed event.
        * 
        * @event NoteSpaceApp#spaceRemoved
-       * @type {object}
-       * @property {number} spaceNum index of removed space.
-       * @property {?NoteSpace} removedSpace object of removed space.
+       * @type {NoteSpace}
        */
-      this.emit("spaceRemoved", {spaceNum, removedSpace})
+      this.emit("spaceRemoved", removedSpace)
 
       return removedSpace
    }
